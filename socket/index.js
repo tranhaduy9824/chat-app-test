2e8665be9120cfa2800aa0f879d7149f4603fcc3
tranhaduy204 <tranhaduy204@gmail.com>
2024-07-02 14:35:58 +0700

    change

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ const { Server } = require("socket.io");
 
 const io = new Server({
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:5000"],
     methods: ["GET", "POST"],
   },
 });
