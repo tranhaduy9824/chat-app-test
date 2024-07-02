82e9e783b4a64c40c50c060e6ab01c05b7500f3c
tranhaduy204 <tranhaduy204@gmail.com>
2024-07-02 10:23:26 +0700

    deploy

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
 const { Server } = require("socket.io");
 
-const io = new Server({ cors: "http://localhost:5173" });
+const io = new Server({ cors: "http://localhost:5000" });
 
 let onlineUsers = [];
 
