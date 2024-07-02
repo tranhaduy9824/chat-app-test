efe1d9d6275c583b54ecb20ad0508939b5d27d90
tranhaduy204 <tranhaduy204@gmail.com>
2024-07-02 23:20:47 +0700

    change

--- socket/index.js
+++ socket/index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-test-1sr1.onrender.com",
+    origin: "https://chat-app-test-1.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
