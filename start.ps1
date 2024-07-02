fd0585db429d9e62eb1ee077d4d8cecd9cf47f3f
tranhaduy204 <tranhaduy204@gmail.com>
2024-07-02 14:02:13 +0700

    change

--- start.ps1
+++
@@ -1,5 +0,0 @@
-# Chạy npm start trong cửa sổ PowerShell hiện tại
-npm start
-
-# Mở một cửa sổ PowerShell mới và chạy npm run socket
-Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -NoProfile -File "./run-socket.ps1"' -Verb RunAs
